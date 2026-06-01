2b77e0af856d80f02703265c32e1130a39139c87
joesimeone <joesimeone9@gmail.com>
2026-06-01 09:59:05 -0400

    Added state_name values to partitioned parquet files

--- helpers/code/break_up_big_files.R
+++ helpers/code/break_up_big_files.R
@@ -3,8 +3,7 @@ con <- dbConnect(duckdb(), dbdir = "ucmr_shiny.db")
 
 dir.create("export", showWarnings = FALSE)
 
-tables <- c("tract_boundaries", 
-             "wgt_tract_contam", "ucmr_og_tract_contam", "pws_wgt_tract_contam")
+tables <- c("wgt_tract_contam", "ucmr_og_tract_contam", "pws_wgt_tract_contam")
 
 for (tbl in tables) {
   dbExecute(con, sprintf(
@@ -13,8 +12,27 @@ for (tbl in tables) {
 }
 
 dbExecute(con, "
-  COPY epa_water_boundaries TO 'export/epa_water_parts' 
-  (FORMAT PARQUET, COMPRESSION ZSTD, PARTITION_BY (Primacy_Agency))
+  COPY (
+    SELECT e.PWSID, e.geom_wkb, e.Detailed_Facility_Report, e.Model_Method, u.state_name
+    FROM epa_water_boundaries e
+    INNER JOIN (SELECT DISTINCT PWSID, state_name FROM ucmr_og_tract_contam) u
+      ON e.PWSID = u.PWSID
+  ) TO 'export/epa_water_parts'
+  (FORMAT PARQUET, COMPRESSION ZSTD, PARTITION_BY (state_name))
 ")
 
+dbExecute(
+  con, "
+  COPY (
+    SELECT tb.*, wt.state_name
+    FROM tract_boundaries tb
+    LEFT JOIN (
+      SELECT DISTINCT tract_geoid, state_name
+      FROM wgt_tract_contam
+    ) wt ON tb.GEOID = wt.tract_geoid
+  ) TO 'export/tract_boundaries'
+  (FORMAT PARQUET, COMPRESSION ZSTD, PARTITION_BY (state_name))"
+)
+
 dbDisconnect(con)
+
